2067092e2f52628d1ea10c3c0f176c11bbd72ebb
Blamkauy <27.leonardo.ehm@ksd.org>
2026-01-08 09:33:09 -0800

    Implemented 3d "Sprite Objects"
    use these to put objects into the scene.

---
+++ GroupMobileGame/Assets/SpriteObject.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class SpriteObject : MonoBehaviour
+{
+    public Vector3 position;//X and Z for baseic 2D directions, Y for vertical. Assume 3D math for all transformations and scene queries
+    public bool setPosition = true;
+    public bool setSprite = true;
+    public bool setSpriteDepth = true;
+
+    public Sprite[] sprites;//All frames of every angle in order of Angle1Frame1, Angle1Frame2,Angle1Frame3,Angle2Frame1,Angle2Frame2... etc.
+    public int rotationCount;//How many different angles are there?
+    public int currentFrame;//Curent animation frame
+    public Sprite overrideSprite;//normall null, otherwise, will always set the spriterenderer to this sprite.
+    public SpriteRenderer sr;
+    public float angle;//Verticle angle.
+    private void OnDrawGizmos()
+    {
+        UpdateSprite();
+    }
+    public void UpdateSprite()
+    {
+        if(setPosition)
+        {
+            transform.position = new Vector3(position.x,position.z+position.y*0.422f, 0);//sin 25° ~= 0.422
+        }
+
+        if (sr == null||!setSprite) return;
+        if(setSpriteDepth)
+        {
+            sr.sortingOrder = Mathf.RoundToInt(-position.z);
+        }
+        if (overrideSprite != null)
+        {
+            sr.sprite = overrideSprite;
+        }
+        else
+        {
+            if (sprites == null || sprites.Length <= 0) return;
+            float truncatedAngle = angle / 360f % 1;
+            while (truncatedAngle < 0) truncatedAngle++;
+            sr.sprite = rotationCount == 0||sprites.Length>=rotationCount ? sprites[currentFrame] : sprites[Mathf.RoundToInt(truncatedAngle * rotationCount) * sprites.Length / rotationCount + currentFrame];
+        }
+    }
+    void Update()
+    {
+        UpdateSprite();
+    }
+}
